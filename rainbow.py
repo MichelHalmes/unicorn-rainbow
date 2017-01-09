ee620158eff1ce72cd47faf74aa3c044f7c14b13
MichelHalmes <michel.halmes@gmail.com>
2017-01-09 17:14:08 +0200

    refactor

--- rainbow.py
+++ rainbow.py
@@ -15,55 +15,57 @@ LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
 LED_BRIGHTNESS = 80     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-LED_SIZES      =  {'red':       {'offset': 2, 'length': 3, 'idx': 0, 'basecolor': (255,0,0)},
-                    'orange':   {'offset': 2, 'length': 4, 'idx': 1, 'basecolor': (128,128,0)},
-                    'yellow':   {'offset': 2, 'length': 5, 'idx': 2, 'basecolor': (10,155,0)},
-                    'green':    {'offset': 2, 'length': 6, 'idx': 3, 'basecolor': (0,255,0)},
-                    'blue':     {'offset': 2, 'length': 7, 'idx': 4, 'basecolor': (0,0,255)},
-                    'violet':   {'offset': 2, 'length': 8, 'idx': 5, 'basecolor': (50,50,255)}
-                    }
+RAINBOW_PARTS  =  [
+    ('red',      {'offset': 2, 'length': 3, 'basecolor': (255,0,0)}),
+    ('orange',   {'offset': 2, 'length': 4, 'basecolor': (128,128,0)}),
+    ('yellow',   {'offset': 2, 'length': 5, 'basecolor': (10,155,0)}),
+    ('green',    {'offset': 2, 'length': 6, 'basecolor': (0,255,0)}),
+    ('blue',     {'offset': 2, 'length': 7, 'basecolor': (0,0,255)}),
+    ('violet',   {'offset': 2, 'length': 8, 'basecolor': (50,50,255)})
+]
+                    
 
 class Rainbow(object):
     def __init__(self):
 
-        
-
-
-        self._repr = OrderedDict([('red', []), ('orange', []), ('yellow', []), ('green', []), ('blue', []), ('violet', [])])
+        self._parts = []
         start_idx = 0
-        for spectrum, led_rgbs in self._repr.items():
-            print spectrum
-            rgb = LED_SIZES[spectrum]['basecolor']
-            length = LED_SIZES[spectrum]['length']
-            start_idx += LED_SIZES[spectrum]['offset']
-            LED_SIZES[spectrum]['start_idx'] = start_idx
-            start_idx +=length
+        for name, parts_dict in RAINBOW_PARTS:
+            start_idx += parts_dict['offset']
+            part = Parts(name, start_idx, parts_dict['length'], parts_dict['basecolor'])
+            start_idx += parts_dict['length']
             
-            self._repr[spectrum] = [rgb]*length
+            self._parts.append(part)
 
 
         self._strip = Adafruit_NeoPixel(start_idx, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
         self._strip.begin()
 
-        for spectrum, led_rgbs in self._repr.items():
-            start = LED_SIZES[spectrum]['start_idx']
-            end = start + LED_SIZES[spectrum]['length']
-            self._strip.setPixelColor(slice(start,end)  , map(lambda  rgb: Color(rgb[0], rgb[1], rgb[2]), led_rgbs))
-
+    def render_parts(self):
+        for part in self._parts:
+            part.render(self._strip)
+           
         self._strip.show()
 
             
 
+class Part(object):
+    def __init__(self, name, start_idx, length, base_rgb):
+        self._name = name
+        self._start_idx = start_idx
+        self._length = length
+        self._leds_rgb = [base_rgb] * length
 
-
-
-
-        
+    def render(self, strip):
+        start = self._start_idx
+        end = start + self._length
+        strip.setPixelColor(slice(start,end), map(lambda  rgb: Color(*rgb), self._led_rgbs))
 
 
 
 
 # Main program logic follows:
 if __name__ == '__main__':
-    Rainbow()
+    Rainbow().render_parts()
+
 
