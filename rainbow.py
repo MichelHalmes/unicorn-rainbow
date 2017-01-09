acaed2baa5024cd81ac5c141aa693d032930ebe0
MichelHalmes <michel.halmes@gmail.com>
2017-01-09 15:42:15 +0200

    basic rainbow

--- rainbow.py
+++ rainbow.py
@@ -5,94 +5,58 @@
 # various animations on a strip of NeoPixels.
 import time
 
-from neopixel import *
+from rpi_ws281x.python.neopixel import Color, Adafruit_NeoPixel
+from collections import OrderedDict
 
 
 # LED strip configuration:
-LED_COUNT      = 60      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
 LED_BRIGHTNESS = 80     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_SIZES      =  {'red':       {offset: 2, length: 10, idx: 0, basecolor: (255,0,0)},
+                    'orange':   {offset: 2, length: 10, idx: 1, basecolor: (128,128,0)},
+                    'yellow':   {offset: 2, length: 10, idx: 2, basecolor: (10,155,0)},
+                    'green':    {offset: 2, length: 10, idx: 3, basecolor: (0,255,0)},
+                    'blue':     {offset: 2, length: 10, idx: 4, basecolor: (0,0,255)},
+                    'violet':   {offset: 2, length: 10, idx: 5, basecolor: (50,50,255)}
+                    }
+
+class Rainbow(object):
+    def __init__():
+
+        self._strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+        self._strip.begin()
 
 
-# Define functions which animate LEDs in various ways.
-def colorWipe(strip, color, wait_ms=50):
-	"""Wipe color across display a pixel at a time."""
-	for i in range(strip.numPixels()):
-		strip.setPixelColor(i, color)
-		strip.show()
-		time.sleep(wait_ms/1000.0)
+        self._repr = OrderedDict([('red', []), ('orange', []), ('yellow', []), ('green', []), ('blue', []), ('violet', [])])
+        start_idx = 0
+        for spectrum, led_rgbs in self._repr.items():
+            print spectrum
+            rgb = LED_SIZES[spectrum]['basecolor']
+            length = LED_SIZES[spectrum]['basecolor']
+            start_idx += offset
+            LED_SIZES[spectrum]['start_idx'] = start_idx
+            
+            self._repr[spectrum] = [rgb]*length
 
-def theaterChase(strip, color, wait_ms=50, iterations=10):
-	"""Movie theater light style chaser animation."""
-	for j in range(iterations):
-		for q in range(3):
-			for i in range(0, strip.numPixels(), 3):
-				strip.setPixelColor(i+q, color)
-			strip.show()
-			time.sleep(wait_ms/1000.0)
-			for i in range(0, strip.numPixels(), 3):
-				strip.setPixelColor(i+q, 0)
 
-def wheel(pos):
-	"""Generate rainbow colors across 0-255 positions."""
-	if pos < 85:
-		return Color(pos * 3, 255 - pos * 3, 0)
-	elif pos < 170:
-		pos -= 85
-		return Color(255 - pos * 3, 0, pos * 3)
-	else:
-		pos -= 170
-		return Color(0, pos * 3, 255 - pos * 3)
+            for rgb in led_rgbs:
+                self._strip.setPixelColor(slice(start_idx,start_idx+length), map(lambda  rgb: Color(rgb[0], rgb[1], rgb[3]), self._repr[spectrum]))
 
-def rainbow(strip, wait_ms=20, iterations=1):
-	"""Draw rainbow that fades across all pixels at once."""
-	for j in range(256*iterations):
-		for i in range(strip.numPixels()):
-			strip.setPixelColor(i, wheel((i+j) & 255))
-		strip.show()
-		time.sleep(wait_ms/1000.0)
+            start_idx +=length
 
-def rainbowCycle(strip, wait_ms=20, iterations=5):
-	"""Draw rainbow that uniformly distributes itself across all pixels."""
-	for j in range(256*iterations):
-		for i in range(strip.numPixels()):
-			strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
-		strip.show()
-		time.sleep(wait_ms/1000.0)
 
-def theaterChaseRainbow(strip, wait_ms=50):
-	"""Rainbow movie theater light style chaser animation."""
-	for j in range(256):
-		for q in range(3):
-			for i in range(0, strip.numPixels(), 3):
-				strip.setPixelColor(i+q, wheel((i+j) % 255))
-			strip.show()
-			time.sleep(wait_ms/1000.0)
-			for i in range(0, strip.numPixels(), 3):
-				strip.setPixelColor(i+q, 0)
+
+
+
+        
+
+
 
 
 # Main program logic follows:
 if __name__ == '__main__':
-	# Create NeoPixel object with appropriate configuration.
-	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
-	# Intialize the library (must be called once before other functions).
-	strip.begin()
+    Rainbow()
 
-	print ('Press Ctrl-C to quit.')
-	while True:
-		# Color wipe animations.
-		colorWipe(strip, Color(255, 0, 0))  # Red wipe
-		colorWipe(strip, Color(0, 255, 0))  # Blue wipe
-		colorWipe(strip, Color(0, 0, 255))  # Green wipe
-		# Theater chase animations.
-		theaterChase(strip, Color(127, 127, 127))  # White theater chase
-		theaterChase(strip, Color(127,   0,   0))  # Red theater chase
-		theaterChase(strip, Color(  0,   0, 127))  # Blue theater chase
-		# Rainbow animations.
-		rainbow(strip)
-		rainbowCycle(strip)
-		theaterChaseRainbow(strip)
