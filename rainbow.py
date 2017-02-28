94ea163c36935e8f50838fdfd0e1cc4b3e5815d1
MichelHalmes <michel.halmes@takelot.com>
2017-02-28 21:48:11 +0200

    New Wheel & double dense part

--- rainbow.py
+++ rainbow.py
@@ -10,18 +10,15 @@ from rpi_ws281x.python.neopixel import Color, Adafruit_NeoPixel
 from collections import OrderedDict
 
 
-def wheel(pos):
-    """Generate rainbow colors across 0-255 positions."""
-    if pos < 85:
-        return (255 - pos * 3, pos * 3, 0)
-    elif pos < 170:
-        pos -= 85
-        return (0, 255 - pos * 3, pos * 3)
-    else:
-        pos -= 170
-        return (pos * 3, 0, 255 - pos * 3)
+from math import cos, pi
+def wheel(deg):
+    red   = cos(deg      *pi/180) * 127 + 128
+    green = cos((deg+120)*pi/180) * 127 + 128
+    blue  = cos((deg+240)*pi/180) * 127 + 128
+    return (red, green, blue)
 
-RAINBOW_RGB = map(wheel, range(256))
+
+RAINBOW_RGB = map(wheel, range(360))
 
 
 " PARTS INITIALIZATION "
@@ -32,12 +29,12 @@ LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
 LED_BRIGHTNESS = 155     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 RAINBOW_PARTS  =  [
-    ('violet',  {'offset': 0, 'length': 37, 'is_reverse': False, 'base_rgb': RAINBOW_RGB[234]}),
-    ('blue',    {'offset': 0, 'length': 32, 'is_reverse': True,  'base_rgb': RAINBOW_RGB[191]}),
-    ('green',   {'offset': 1, 'length': 55,  'is_reverse': False, 'base_rgb': RAINBOW_RGB[149]}),
-    ('yellow',  {'offset': 1, 'length': 24, 'is_reverse': True,  'base_rgb': RAINBOW_RGB[106]}),
-    ('orange',  {'offset': 0, 'length': 20,  'is_reverse': False, 'base_rgb': RAINBOW_RGB[64]}),
-    ('red',     {'offset': 0, 'length': 15,  'is_reverse': True,  'base_rgb': RAINBOW_RGB[21]}),    
+    ('violet',  {'offset': 0, 'length': 37, 'is_reverse': False, 'base_rgb': RAINBOW_RGB[330]}),
+    ('blue',    {'offset': 0, 'length': 32, 'is_reverse': True,  'base_rgb': RAINBOW_RGB[270]}),
+    ('green',   {'offset': 1, 'length': 55, 'is_reverse': False, 'base_rgb': RAINBOW_RGB[210], 'is_double_dense': True}),
+    ('yellow',  {'offset': 1, 'length': 24, 'is_reverse': True,  'base_rgb': RAINBOW_RGB[150]}),
+    ('orange',  {'offset': 0, 'length': 20, 'is_reverse': False, 'base_rgb': RAINBOW_RGB[90]}),
+    ('red',     {'offset': 0, 'length': 15, 'is_reverse': True,  'base_rgb': RAINBOW_RGB[30]}),
 ]
 
 " SPEED PARAMETERS "
@@ -61,7 +58,7 @@ class Rainbow(object):
             part = Part(name, start_idx, part_idx, parts_dict)
             start_idx += parts_dict['length']
             part_idx += 1
-            
+
             self._parts.append(part)
 
         self._strip = Adafruit_NeoPixel(start_idx, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
@@ -71,10 +68,10 @@ class Rainbow(object):
     def render_parts(self):
         for part in self._parts:
             part.render(self._strip)
-           
+
         self._strip.show()
 
-    
+
     def animation(self, anim_func, reset_rgb, nb_cycles=3):
         self._anim_data = {}
         for part in self._parts:
@@ -88,15 +85,15 @@ class Rainbow(object):
             self.render_parts()
             time.sleep(WAIT_MS/1000.0)
 
-    def a_colorwipe(self, part, step): 
+    def a_colorwipe(self, part, step):
         pixel_idx = int(step * part._length / CYCLE_NB_STEPS) % part._length
         if pixel_idx == 0:
             part.set_uniform_color((0,0,0))
 
         part.set_pixel_color(pixel_idx)
 
-    def a_flashparts(self, part, step): 
-        if step % NORMAL_STEP_PERIOD != 0: 
+    def a_flashparts(self, part, step):
+        if step % NORMAL_STEP_PERIOD != 0:
             return
 
         periods = step/NORMAL_STEP_PERIOD
@@ -111,10 +108,10 @@ class Rainbow(object):
         start_idx = step % RAINBOW_LEN
         leds_rgb = [RAINBOW_RGB[int(start_idx + (led_idx*RAINBOW_LEN/part._length))%RAINBOW_LEN] for led_idx in range(part._length)]
         part.set_leds_rgb(leds_rgb)
-       
+
 
     def a_commet(self, part, step):
-        if step % NORMAL_STEP_PERIOD != 0: 
+        if step % NORMAL_STEP_PERIOD != 0:
             return
 
         if len(part._anim_data) == 0:
@@ -151,16 +148,19 @@ class Part(object):
         self._length = part_dict['length']
         self._is_reverse = part_dict['is_reverse']
         self._base_rgb = part_dict['base_rgb']
+        self._is_double_dense = part_dict.get('is_double_dense', False)
         self._leds_rgb = [self._base_rgb] * self._length
         self._anim_data = {}
 
     def render(self, strip):
         start = self._start_idx
         end = start + self._length
+        step = 1 if not self._is_double_dense \
+                else 2
         if self._is_reverse:
-            strip.setPixelColor(slice(start,end), map(lambda rgb: Color(*rgb), reversed(self._leds_rgb)))
+            strip.setPixelColor(slice(start,end, step), map(lambda rgb: Color(*rgb), reversed(self._leds_rgb)))
         else:
-            strip.setPixelColor(slice(start,end), map(lambda rgb: Color(*rgb), self._leds_rgb))
+            strip.setPixelColor(slice(start,end, step), map(lambda rgb: Color(*rgb), self._leds_rgb))
 
 
     def set_uniform_color(self, rgb = None):
@@ -188,5 +188,3 @@ if __name__ == '__main__':
     rainbow.animation(rainbow.a_colorwipe, (0,0,0), 3)
     rainbow.animation(rainbow.a_commet, None, 5)
     rainbow.animation(rainbow.a_flashparts, (0,0,0), 5)
-
-
